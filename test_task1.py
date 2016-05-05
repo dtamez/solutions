392c11beb0eca3cbcf9011a768185828ca9df1dc
Danny Tamez <zematynnad@gmail.com>
2016-05-05 10:17:09 -0500

    GREEN: make_move raises error when trying to move left from column a

--- test_task1.py
+++ test_task1.py
@@ -48,3 +48,8 @@ class TestTask1(unittest.TestCase):
 
         expected = 0, 6
         self.assertEqual(move, expected)
+
+    def test_move_left_a1(self):
+
+        expected = task1.NoMoveError
+        self.assertRaises(expected, task1.make_move, 0, 0, task1.LEFT)
