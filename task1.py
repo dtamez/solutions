52793300235cc4dba8906e4d58a155c2031c9381
Danny Tamez <zematynnad@gmail.com>
2016-05-05 15:28:22 -0500

    REFACTOR: a few comments and re-org

--- task1.py
+++ task1.py
@@ -15,6 +15,8 @@ from __future__ import absolute_import
 
 import argparse
 
+# Some constants to avoid typos, and make the code easier to read
+# COLS and COLS_REVERSE are for switcing between indices and algebraic columns
 UP, RIGHT, LEFT, DOWN, UP_RIGHT, DOWN_RIGHT, DOWN_LEFT, UP_LEFT = range(8)
 COLS = {n: l for n, l in zip(range(8), list('abcdefgh'))}
 COLS_REVERSE = {v: k for k, v in COLS.items()}
@@ -34,6 +36,53 @@ class IllegalPositionError(Exception):
     pass
 
 
+def get_available_moves(piece, position):
+    col, row = from_algebraic(position)
+    moves = []
+    if piece == PAWN:
+        moves = get_pawn_moves(col, row)
+    elif piece == ROOK:
+        moves = get_rook_moves(col, row)
+    elif piece == BISHOP:
+        moves = get_bishop_moves(col, row)
+    elif piece == QUEEN:
+        moves = get_queen_moves(col, row)
+    elif piece == KING:
+        moves = get_king_moves(col, row)
+    elif piece == KNIGHT:
+        moves = get_knight_moves(col, row)
+
+    for idx, move in enumerate(moves):
+        moves[idx] = to_algebraic(*move)
+    return moves or 'No moves are available.'
+
+
+class Moves(object):
+
+    def __init__(self, col, row, direction, limit=8):
+        self.col = col
+        self.row = row
+        self.direction = direction
+        self.limit = limit
+        self.num_returned = 0
+
+    def next(self):
+        if self.num_returned >= self.limit:
+            raise StopIteration
+        try:
+            move = make_move(self.col, self.row, self.direction)
+            self.col, self.row = move
+            self.num_returned += 1
+        except NoMoveError:
+            raise StopIteration
+
+        return move
+
+    def __iter__(self):
+        return self
+
+
+# Primitives for moving one square in a given direction
 def make_move(col, row, direction):
     if direction == UP:
         return move_up(col, row)
@@ -81,6 +130,7 @@ def move_right(col, row):
         return col + 1, row
 
 
+# moves for specific pieces
 def get_pawn_moves(col, row):
     if row == 0:
         msg = 'This is not a valid position for a pawn.'
@@ -148,6 +198,8 @@ def get_knight_moves(col, row):
     return [move for move in moves if move]
 
 
+# helper functions for switching back and forth
+# between algebraic notation and col, row format
 def from_algebraic(position):
     col = COLS_REVERSE[position[0]]
     row = int(position[1]) - 1
@@ -156,52 +208,6 @@ def from_algebraic(position):
 
 def to_algebraic(col, row):
     return '{}{}'.format(COLS[col], row + 1)
-
-
-def get_available_moves(piece, position):
-    col, row = from_algebraic(position)
-    moves = []
-    if piece == PAWN:
-        moves = get_pawn_moves(col, row)
-    elif piece == ROOK:
-        moves = get_rook_moves(col, row)
-    elif piece == BISHOP:
-        moves = get_bishop_moves(col, row)
-    elif piece == QUEEN:
-        moves = get_queen_moves(col, row)
-    elif piece == KING:
-        moves = get_king_moves(col, row)
-    elif piece == KNIGHT:
-        moves = get_knight_moves(col, row)
-
-    for idx, move in enumerate(moves):
-        moves[idx] = to_algebraic(*move)
-    return moves or 'No moves are available.'
-
-
-class Moves(object):
-
-    def __init__(self, col, row, direction, limit=8):
-        self.col = col
-        self.row = row
-        self.direction = direction
-        self.limit = limit
-        self.num_returned = 0
-
-    def next(self):
-        if self.num_returned >= self.limit:
-            raise StopIteration
-        try:
-            move = make_move(self.col, self.row, self.direction)
-            self.col, self.row = move
-            self.num_returned += 1
-        except NoMoveError:
-            raise StopIteration
-
-        return move
-
-    def __iter__(self):
-        return self
 
 
 if __name__ == "__main__":
